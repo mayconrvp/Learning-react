0fee48e038c134deb2cc57e6987495859aa6b20a
Maycon <maycon.rvp@outlook.com>
2021-11-04 22:40:01 -0300

    Conceitos de navegação com React Router

---
+++ navegacao/src/views/examples/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const About = (props) => (
+  <div className="about">
+    <h1>Sobre</h1>
+    <p>
+      Lorem ipsum dolor sit amet. Qui necessitatibus consequatur et natus rerum
+      et voluptatum dolor et velit adipisci 33 minus nihil ut eveniet illo? A
+      sunt harum ut fugit culpa qui consequatur commodi. Vel eveniet dolorum qui
+      officia sint ab dignissimos doloremque nam illum eum laboriosam quidem est
+      velit quisquam 33 doloribus repellat. In quia velit quo deleniti itaque At
+      autem galisum et possimus dolor ea incidunt ipsa. Optio eius qui rerum
+      dolorem ut odio voluptate sit quasi impedit sit molestiae ipsam id velit
+      galisum aut minus saepe!
+    </p>
+    <p>
+      Quo molestiae modi ut quod excepturi sed quia cupiditate rem doloremque
+      magnam in possimus rerum non sunt consequatur. Ut debitis dolor et
+      consequatur totam ut laborum harum internos voluptatem non praesentium
+      aliquam. Ut corporis omnis eum harum fugiat At ipsa facere. Cum enim nulla
+      est illum ullam 33 autem Quis qui officiis autem est incidunt nulla. Qui
+      nulla nesciunt vel adipisci quia et quae labore ut odit nobis. Ab
+      architecto reiciendis qui sint tempore sunt dignissimos est molestias
+      omnis sit eligendi doloremque! Aut beatae officiis qui sunt inventore quo
+      omnis minus qui eveniet velit nam culpa consequatur qui ipsum sunt est
+      voluptatem eaque.
+    </p>
+    <p>
+      Et suscipit atque qui perferendis quasi eum cumque beatae est asperiores
+      quos! Aut sapiente voluptate aut neque rerum et consequatur earum hic enim
+      neque et quae voluptate et magnam voluptatem et placeat amet. Aut quas
+      molestias aut optio corporis in officia placeat et quod perspiciatis? Quo
+      molestias sint est quos temporibus est atque quis est dolorum voluptas? Ea
+      fugit illum non consequatur incidunt et Quis molestias et cupiditate nihil
+      sed deleniti galisum qui molestiae consequatur a asperiores praesentium.
+      Et saepe voluptatem in odio earum sit facere optio qui enim enim. Non
+      quidem dolores vel ducimus libero ut aspernatur quisquam quam voluptate
+      est molestiae modi non expedita illum? Ut sapiente expedita id aspernatur
+      dolorem et odio temporibus aut repellat dolores ut mollitia voluptatum est
+      eligendi perferendis? Aut itaque veniam est molestias reprehenderit et
+      beatae corporis et sunt molestiae ut cumque exercitationem ut itaque
+      ipsam.
+    </p>
+  </div>
+);
+
+export default About;
---
+++ navegacao/src/views/examples/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = (props) => (
+  <div className="notFound">
+    <h1>404</h1>
+    <p>
+      <h2>Opsss... Página não encontrada!</h2>
+    </p>
+  </div>
+);
+
+export default NotFound;
